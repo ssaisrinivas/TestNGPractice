9336b18be3a9ad98587bb5600ac241a220e9c5cb
ssais <ssais@sai>
2025-03-22 15:05:28 +0530

    aa

--- TestNGPractice/src/main/java/com/testng/base/BaseClass.java
+++ TestNGPractice/src/main/java/com/testng/base/BaseClass.java
@@ -4,14 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -107,12 +110,69 @@ public class BaseClass {
     @Test
     public void Test() throws InterruptedException {
 	// Loading Orange HRM Home page
-	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-	Thread.sleep(3000);
-	driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
-	driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
-	driver.findElement(By.xpath("//button[@type='submit']")).click();
+	driver.get("https://sourceforge.net/auth/");
+	Thread.sleep(1000);
+	String loginpageTitle = driver.getTitle();
+	System.out.println(loginpageTitle);
+	Thread.sleep(1000);
+	System.out.println(driver.findElement(By.xpath("//a[text()='Forgot your username/password?']")).isDisplayed());
+	driver.findElement(By.cssSelector("[placeholder=\"Username\"]")).sendKeys("ssaisrinivas22");
+	driver.findElement(By.cssSelector("[placeholder=\"Password\"]")).sendKeys("$Ss9704312287");
+	driver.findElement(By.cssSelector("[type=\"submit\"][value=\"Login\"]")).click();
+	Thread.sleep(1000);
+	String accountTitle = driver.getTitle();
+	System.out.println(accountTitle);
+	Thread.sleep(1000);
+	Actions action = new Actions(driver);
+	action.moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Me ▾')]"))).perform();
+	Thread.sleep(1000);
+	driver.findElement(By.xpath("//ul[@id='account-menu']//a[normalize-space()='Account Settings']")).click();
+	Thread.sleep(1000);
+	List<WebElement> webelements = driver.findElements(By.cssSelector("fieldset.preferences a"));
+	for (WebElement webElement : webelements) {
+	    System.out.println(webElement.getAccessibleName());
+	}
 
+	Thread.sleep(1000);
+
+	// OAuth Page
+
+	driver.findElement(By.xpath("//*[@id='account_oauth']/a")).click();
+
+	boolean value = driver.getPageSource().contains("Sai Srinivas Git ");
+
+	if (value) {
+	    driver.findElement(By.xpath("//input[@value='Delete App']")).click();
+	    driver.switchTo().alert().accept();
+	}
+
+	else {
+	    driver.findElement(By.xpath("//input[@name='application_name'][@tabindex='6']")).sendKeys("Sai Srinivas");
+	    Thread.sleep(3000);
+
+	    driver.findElement(By.xpath("//textarea[@name='application_description'][@tabindex='7']"))
+		    .sendKeys("These are applications you have authorized to act on your behalf.");
+
+	    driver.findElement(By.xpath("//input[@name='redirect_url_1'][@tabindex='8']"))
+		    .sendKeys("https://github.com/ssaisrinivas?tab=repositories");
+	    driver.findElement(By.xpath("//input[@name='redirect_url_2'][@tabindex='9']"))
+		    .sendKeys("https://github.com/ssaisrinivas?tab=repositories");
+	    driver.findElement(By.xpath("//input[@name='redirect_url_3'][@tabindex='10']"))
+		    .sendKeys("https://github.com/ssaisrinivas?tab=repositories");
+	    Thread.sleep(8000);
+	    driver.findElement(By.xpath("//input[@value='Register new Application']")).click();
+	    Thread.sleep(5000);
+	    String name = driver.findElement(By.cssSelector("div > div.content")).getText();
+	    System.out.println(name);
+
+	    Thread.sleep(5000);
+	    driver.findElement(By.xpath("//input[@value='Delete App']")).click();
+	    driver.switchTo().alert().accept();
+	}
+
+	System.out.println();
+	System.out.println(value);
+	Thread.sleep(8000);
     }
 
     @AfterMethod
